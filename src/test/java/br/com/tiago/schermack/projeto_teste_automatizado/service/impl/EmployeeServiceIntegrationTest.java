01c56555d0517c4cf76cf3b4a51a3c1ce5c921b7
Your Name <you@example.com>
2026-02-24 19:36:37 -0300

    Feat: Atualizar fuincinario, update de funcinario, deletar funcinario.

--- src/test/java/br/com/tiago/schermack/projeto_teste_automatizado/service/impl/EmployeeServiceIntegrationTest.java
+++ src/test/java/br/com/tiago/schermack/projeto_teste_automatizado/service/impl/EmployeeServiceIntegrationTest.java
@@ -1,6 +1,7 @@
 package br.com.tiago.schermack.projeto_teste_automatizado.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.tiago.schermack.projeto_teste_automatizado.dto.EmployeeRequestDTO;
 import br.com.tiago.schermack.projeto_teste_automatizado.dto.EmployeeResponseDTO;
+import br.com.tiago.schermack.projeto_teste_automatizado.entity.Employee;
 import br.com.tiago.schermack.projeto_teste_automatizado.repository.EmployeeRepository;
 import jakarta.transaction.Transactional;
 
@@ -60,8 +62,26 @@ public class EmployeeServiceIntegrationTest {
         EmployeeResponseDTO response = employeeService.update(responseDTO.id(), updateRequesteDTO);
 
         //Assert
+        assertEquals(response.firstName(),"Wellinton Braz");
+        assertEquals(response.email(),"test@gmal");
+        assertEquals(response.id(), 1L);
 
         }
+        @Test
+        @DisplayName("Deve deletar o funcinario existente e retonar sucesso")
+                //arrange
+        public void deveDeletarFuncinarioExistenteERetonarSucesso(){
+                var requestDTO = new EmployeeRequestDTO(
+                        "Wellinton Braz",
+                        "test@gmal");
+                var responseDTO = employeeService.create(requestDTO);
 
+                //act
+                employeeService.delete(responseDTO.id());
+
+                //assert
+                Employee employeeDeleted = employeeRepository.findById(responseDTO.id()).orElse(null);
+                assertNull(employeeDeleted);
+        }
 
 }
